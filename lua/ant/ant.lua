c4b32b0d78f18116e287bcf978a040e42c2bb5f5
Anthiticpy <74250063+Anthiticpy@users.noreply.github.com>
2026-02-26 00:28:22 -0500

    Trust the Process

--- lua/ant/ant.lua
+++ lua/ant/ant.lua
@@ -1,1 +1,13 @@
 -- your SHIT goes here:grin:
+
+
+-- don't worry about this for now trust the process
+
+notes = P1:GetNoteData(132,163)
+set {132, 3, 'xmod'}
+for i,v in pairs(notes) do
+ease {v[1], 2.5, flip(outCirc), -200, 'wave', 100, 'brake'}
+ease {v[1], 1, flip(outElastic:params(8.,.3)), -450, 'tinyx'}
+
+end
+
